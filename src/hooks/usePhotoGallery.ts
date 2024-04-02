406612090dbdbef12d5640160acc1d396e3ebc6b
h600853 <h600853@stud.hvl.no>
2024-04-02 14:15:22 +0200

    write to filesystem

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -1,6 +1,5 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { isPlatform } from "@ionic/react";
-
 import {
   Camera,
   CameraResultType,
@@ -8,27 +7,42 @@ import {
   Photo,
 } from "@capacitor/camera";
 import { Filesystem, Directory } from "@capacitor/filesystem";
-import { Preferences } from "@capacitor/preferences";
-import { Capacitor } from "@capacitor/core";
 
 export function usePhotoGallery() {
   const [photos, setPhotos] = useState<UserPhoto[]>([]);
 
-  const takePhoto = async () => {
-    const photo = await Camera.getPhoto({
-      resultType: CameraResultType.Uri,
-      source: CameraSource.Camera,
-      quality: 100,
+  const savePicture = async (
+    photo: Photo,
+    fileName: string
+  ): Promise<UserPhoto> => {
+    const base64Data = await base64FromPath(photo.webPath!);
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
     });
-    const fileName = Date.now() + ".jpeg";
-    const newPhotos = [
-      {
-        filepath: fileName,
-        webviewPath: photo.webPath,
-      },
-      ...photos,
-    ];
-    setPhotos(newPhotos);
+
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath,
+    };
+  };
+
+  const takePhoto = async () => {
+    try {
+      const photo = await Camera.getPhoto({
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Camera,
+        quality: 100,
+      });
+
+      const fileName = Date.now() + ".jpeg";
+      const savedFileImage = await savePicture(photo, fileName);
+      const newPhotos = [savedFileImage, ...photos];
+      setPhotos(newPhotos);
+    } catch (error) {
+      console.error("Error taking photo:", error);
+    }
   };
 
   return {
@@ -36,7 +50,25 @@ export function usePhotoGallery() {
     takePhoto,
   };
 }
+
 export interface UserPhoto {
   filepath: string;
   webviewPath?: string;
+}
+
+async function base64FromPath(path: string): Promise<string> {
+  const response = await fetch(path);
+  const blob = await response.blob();
+  return new Promise<string>((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      if (typeof reader.result === "string") {
+        resolve(reader.result);
+      } else {
+        reject("Method did not return a string");
+      }
+    };
+    reader.readAsDataURL(blob);
+  });
 }
